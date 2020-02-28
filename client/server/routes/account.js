a4bd95a6466c7dc867e48f1fbeda4f53cb5713f4
JWadee <58128135+JWadee@users.noreply.github.com>
2020-02-28 18:23:52 -0500

    Sign Up
    Sign Up form is validated, API created and ready for use. Need DB on my laptop.

--- client/server/routes/account.js
+++ client/server/routes/account.js
@@ -2,7 +2,57 @@ var express = require('express');
 var router = express.Router();
 let pool = require('../db/db');
 
-function getAccountTypes(req,res){
+function createAccount(req, res){
+  pool.getConnection(function(err, connection){
+    if(err) {
+      connection.release();
+      res.json({"code": 100, "status": "Error in database connection"});
+      return;
+    }
+    console.log("connected as id: " + connection.threadId);
+
+    let accountSql = "INSERT INTO accounts (strFirstName, strLastName, strUsername, strEmail, intAccountStatusID) "+
+              "VALUES (?,?,?,?,1)";
+
+    let accountPassSql = "INSERT INTO accountpasswords (intAccountID, strPassword) "+
+                         "VALUES (last_insert_id(), ?)";
+
+    let accountValues = [
+      req.body.firstname,
+      req.body.lastname,
+      req.body.username,
+      req.body.email
+    ];
+    
+    let pass = req.body.pass;
+
+    connection.query(accountSql, accountValues, function(err, accRes) {
+      if(err) {
+        connection.release();
+        console.log(err);
+      }else{
+        connection.query(accountPassSql, pass, function(err, accPassRes){
+          if(err) {
+            connection.release();
+            console.log(err);
+          }else{
+            connection.release();
+            res.json({
+              accRes,
+              accPassRes
+            })
+          }
+        })
+      }
+    });    
+    
+    connection.on('error', function(err){
+      res.json({"code": 100, "status": "Error in database connection"});
+    })
+  })
+}
+
+function getTypes(req,res){
   pool.getConnection(function(err, connection){
     if(err) {
       connection.release();
@@ -26,12 +76,69 @@ function getAccountTypes(req,res){
   })
 }
 
+function getByEmail(req,res){
+  pool.getConnection(function(err, connection){
+    if(err) {
+      connection.release();
+      res.json({"code": 100, "status": "Error in database connection"});
+      return;
+    }
+    console.log("connected as id: " + connection.threadId);
+
+    let sql = "SELECT COUNT(*) FROM accounts WHERE strEmail = ?"
+    let email = req.query.email;
+    connection.query(sql, email, function(err, row) {
+      connection.release();
+      if(!err) {
+        res.json(row)
+      }
+    });    
+    
+    connection.on('error', function(err){
+      res.json({"code": 100, "status": "Error in database connection"});
+    })
+  })
+}
+
+function getByUsername(req,res){
+  pool.getConnection(function(err, connection){
+    if(err) {
+      connection.release();
+      res.json({"code": 100, "status": "Error in database connection"});
+      return;
+    }
+    console.log("connected as id: " + connection.threadId);
+
+    let sql = "SELECT COUNT(*) FROM accounts WHERE strUsername = ?"
+    let email = req.query.username;
+
+    connection.query(sql, function(err, row) {
+      connection.release();
+      if(!err) {
+        res.json(row)
+      }
+    });    
+    
+    connection.on('error', function(err){
+      res.json({"code": 100, "status": "Error in database connection"});
+    })
+  })
+}
+
 router.post(('/create'), function(req, res){
     createAccount(req);
 });
 
 router.get('/types', function(req, res) { 
-    getAccountTypes(req, res);
+    getTypes(req, res);
+});
+
+router.get('/byEmail', function(req, res) { 
+  getByEmail(req, res);
+});
+
+router.get('/byUsername', function(req, res) { 
+  getByUsername(req, res);
 });
 
 module.exports = router;
--- client/src/components/pages/SignUp.js
+++ client/src/components/pages/SignUp.js
@@ -1,49 +1,63 @@
 import React, {useState, useEffect} from "react";
 import {Jumbotron, Form, Row, Col, Button} from 'react-bootstrap'
+const validator = require('validator');
 
 const SignUp = () => {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
-    const [gender, setGender] = useState('');
+    //State Variables for form
+    const [firstName, setFirstName] = useState();
+    const [lastName, setLastName] = useState();
     const [username, setUsername] = useState();
-    const [display, setDisplay] = useState();
-
+    const [email, setEmail] = useState();
+    const [pass, setPass] = useState();
+    const [gender, setGender] = useState();
+    const [submit, setSubmit] = useState(false);
+    const [userExists, setUserExists] = useState();
+    
+    async function checkForUsername(){
+        //fetch user by username and store in response
+        const response = await fetch('account/byUsername')
+            .catch((error) => console.log(error))
+        //wait for response and store json   
+        const data = await response.json();    
+        console.log(data);
+         
+    }
+    
     
     return (
         <Jumbotron>
             <h2>Create An Account</h2><br />
             <hr></hr>
-            <Form title="Create An Account"> 
+            <Form title="Create An Account" onSubmit={()=>checkForUsername()}> 
                 <Form.Group as={Row}>
-                    <Form.Label column sm={{span:3, offset:2}} >First Name</Form.Label>
+                    <Form.Label required column sm={{span:3, offset:2}} >First Name</Form.Label>
                     <Col sm={10} md={4} lg={3}>
-                        <Form.Control type="text" onChange={e=> setFirstName(e.target.value)}></Form.Control>
+                        <Form.Control required type="text" minlength="2" onChange={e=> setFirstName(e.target.value)}></Form.Control>
                     </Col>
                 </Form.Group>
                 <Form.Group as={Row} >
                     <Form.Label column sm={{span:3, offset:2}}>Last Name</Form.Label>
                     <Col sm={10} md={4} lg={3}>
-                        <Form.Control type="text" onChange={e=> setLastName(e.target.value)}></Form.Control>
+                        <Form.Control required type="text" minlength="2" onChange={e=> setLastName(e.target.value)}></Form.Control>
                     </Col>
                 </Form.Group>
                 <Form.Group as={Row} >
                     <Form.Label column sm={{span:3, offset:2}}>Username</Form.Label>
                     <Col sm={10} md={4} lg={3}>
-                        <Form.Control type="text" onChange={e=> setUsername(e.target.value)}></Form.Control>
+                        <Form.Control required type="text" minlength="2" onChange={e=> setUsername(e.target.value)}></Form.Control>
                     </Col>
                 </Form.Group>
+                {userExists}
                 <Form.Group as={Row} >
-                    <Form.Label column sm={{span:3, offset:2}} >Email Address</Form.Label>
+                    <Form.Label column sm={{span:3, offset:2}}>Email Address</Form.Label>
                     <Col sm={10} md={4} lg={3}>
-                        <Form.Control type="email" onChange={e=> setEmail(e.target.value)}></Form.Control>
+                        <Form.Control required type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" onChange={e=> setEmail(e.target.value)}></Form.Control>
                     </Col>
                 </Form.Group>
                 <Form.Group as={Row}>
-                    <Form.Label column sm={{span:3, offset:2}}>Phone Number</Form.Label>
+                    <Form.Label column sm={{span:3, offset:2}}>Password</Form.Label>
                     <Col sm={10} md={4} lg={3}>
-                        <Form.Control type="text" onChange={e=> setPhoneNumber(e.target.value)}></Form.Control>
+                        <Form.Control title="Minimum: 8 chars, 1 uppercase, 1 lowercase, 1 number, 1 special" required type="password" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" minlength="8" onChange={e=> setPass(e.target.value)}></Form.Control>
                     </Col>
                 </Form.Group>
                 <Form.Group as={Row}>
@@ -51,22 +65,25 @@ const SignUp = () => {
                         <Form.Check 
                             inline
                             type="radio"
+                            required
                             label="Male"
                             name="gender"
                             value="1"
+                            onChange={(e)=>setGender(e.target.value)}
                         />
-                        <Form.Check
+                        <Form.Check 
                             inline
                             type="radio"
                             label="Female"
                             name="gender"
                             value="2"
+                            onChange={(e)=>setGender(e.target.value)}
                         />
                     </Col>
                 </Form.Group>
                 <Form.Group as={Row}>
                     <Col sm={{ span: 6, offset: 3  }}>
-                        <Button primary onClick={()=> alert("Creating Account")} type="submit">Create Account</Button>
+                        <Button variant="primary" type="submit">Create Account</Button>
                     </Col>
                 </Form.Group>
                 <Form.Group as={Row}>
