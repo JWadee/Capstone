2d506d925570822e8b18b744a4dbe3439fe0ceb6
jcovey-0316 <60574627+jcovey-0316@users.noreply.github.com>
2020-03-22 19:12:01 -0400

    add routine routes

---
+++ client/server/routes/routines.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var router = express.Router();
+let pool = require('../db/db');
+
+
+function addRoutine(req, res) {
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            connection.release();
+            res.json({ "code": 100, "status": "Error in database connection" });
+            return;
+        }
+        console.log("connected as id: " + connection.threadId);
+
+        let sql = "INSERT INTO routines (strRoutineName, intOwnerID) " +
+            "VALUES (?, ?)";
+        let values = [req.body.name, req.body.ownerid];
+
+        connection.query(sql, values, function (err, result) {
+            connection.release();
+            if (!err) {
+                console.log(result)
+                res.json("Success")
+            }
+            console.log(err)
+
+        });
+        connection.on('error', function (err) {
+            res.json({ "code": 100, "status": "Error in database connection" });
+        })
+    })
+}
+
+function getRoutines(req, res) {
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            connection.release();
+            res.json({ "code": 100, "status": "Error in database connection" });
+            return;
+        }
+        console.log("connected as id: " + connection.threadId);
+
+        let sql = "SELECT * FROM routines";
+
+        connection.query(sql, function (err, rows) {
+            connection.release();
+            if (!err) {
+                res.json(rows)
+            }
+        });
+
+        connection.on('error', function (err) {
+            res.json({ "code": 100, "status": "Error in database connection" });
+        })
+    })
+}
+
+router.post(('/add'), function (req, res) {
+    addRoutine(req, res);
+});
+
+router.get(('/'), function (req, res) {
+    getRoutines(req, res);
+});
+
+module.exports = router;
